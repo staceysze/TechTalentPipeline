c96e035831dec8891c2d7b36d13239e93b06fb48
staceysze <staceysze@gmail.com>
2016-06-03 14:12:25 -0400

    Second Commit

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-function checkpassword{
-  if(!(document.getElementById("password").value == "12345678"){
+function processform(){
+  if(document.getElementById("password").value !== "12345678"){
     alert("Wrong Password!");
   }
-}
+
+  var valid = new RegExp("\\d");
+  var username = document.getElementById("username").value;
+    if(!valid.test(username)){
+      alert("Username needs to have at least one digit.")
+    } else{
+      document.getElementById("title").innerText="Hello World!";
+    }
+    return false;
+  }
